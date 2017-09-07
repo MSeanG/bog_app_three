206b8dc8c80d3bcea3644294d5509ef9eb7bbb63
Sean <sgilmore@michael-sean.com>
2017-09-07 13:18:18 -0400

    ran db creat migrate and seed

---
+++ db/migrate/20170907164512_create_creatures.rb
@@ -0,0 +1,10 @@
+class CreateCreatures < ActiveRecord::Migration[5.1]
+  def change
+    create_table :creatures do |t|
+      t.string :name
+      t.string :description
+      t.string :image_url
+      t.timestamps
+    end
+  end
+end
